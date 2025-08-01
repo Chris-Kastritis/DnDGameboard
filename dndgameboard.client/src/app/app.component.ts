9041ef2fcce60b5b65a9c7745fd00599c8efc6d4
chris-kastritis <christopher.kastritis@gmail.com>
2025-08-01 13:45:18 -0400

    V0.2 added game piece functionality

--- dndgameboard.client/src/app/app.component.ts
+++ dndgameboard.client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, QueryList } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +8,27 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css',
 })
+
 export class AppComponent{
   max = 25;
   min = 5;
   value = 5;
+  circles = 0;
+  name: string = "";
+  previewUrl: string | null = null;
+  imageUrl: string [] = [];
   numbers: Array<number> = Array(Math.pow(this.value, 2)).fill(0).map((x, i) => i);
   onSlide() {
     this.numbers = Array(Math.pow(this.value,2)).fill(0).map((x, i) => i);
   }
   calculatedWidth: string = '120px';
   currentScale = 1;
-
+  pieces: number = 0;
   scale: number = 1;  // Initial scale value
   minScale: number = 1;  // Minimum scale
   maxScale: number = 4;  // Maximum scale
   transformOrigin: string = '50% 50%';  // Initial transform origin (center)
-
+  dotImages: string[] = [];
   // Mouse position relative to the container
   mouseX: number = 0;
   mouseY: number = 0;
@@ -37,12 +42,14 @@ export class AppComponent{
   right: number = 0;
   top: number = 0;
   bottom: number = 0;
-
+  circleClicked: number = -1;
   offsetX1: number = 0; // X offset for dragging
   offsetY1: number = 0;
-
+  hasScrolled: boolean = false;
+  dotClicked: boolean[] = [];
   // Handle the mouse scroll event
-   onScroll(event: WheelEvent): void {
+  onScroll(event: WheelEvent): void {
+    this.hasScrolled = true;
     event.preventDefault();  // Prevent default scroll behavior
 
     const delta = event.deltaY > 0 ? -0.1 : 0.1;  // Determine scroll direction
@@ -70,13 +77,17 @@ export class AppComponent{
 
   // Handle the mouse move event to track mouse position
   onMouseMove(event: MouseEvent): void {
-    
     if (this.isDragging) {
       const container = document.querySelector('.zoom-container') as HTMLElement;
       const rect = container.getBoundingClientRect();
       const board = document.querySelector('.game-board') as HTMLElement;
       const boardRect = board.getBoundingClientRect();
-
+      if (!this.hasScrolled) {
+        this.left = boardRect.left;
+        this.right = boardRect.right;
+        this.bottom = boardRect.bottom;
+        this.top = boardRect.top;
+      }
       const box = document.querySelector('.box') as HTMLElement;
       const boxsize = box.getBoundingClientRect();
       const dx = event.clientX - this.startX;
@@ -99,16 +110,6 @@ export class AppComponent{
       this.offsetY = newOffsetY;
 
       // Update start positions to continue dragging from the new position
-      this.startX = event.clientX;
-      this.startY = event.clientY;
-    }
-    else if (this.isDragging1) {
-      const dx = event.clientX - this.startX;
-      const dy = event.clientY - this.startY;
-      let newOffsetX = this.offsetX1 + dx;
-      let newOffsetY = this.offsetY1 + dy;
-      this.offsetX1 = newOffsetX;
-      this.offsetY1 = newOffsetY;
       this.startX = event.clientX;
       this.startY = event.clientY;
     }
@@ -137,6 +138,77 @@ export class AppComponent{
     this.isDragging = false;
     this.isDragging1 = false;
   }
+  onCircleClick(index: number): void {
+    this.circleClicked = index;
+  }
+  onDotClick(number: number): void {
+    this.dotClicked[number] = true;
+  }
+  onBoxClick(number: number): void {
+    const dots: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+    const dot = dots[number];
+   
+    if (this.circleClicked >= 0) {
+      const circle: HTMLElement | null = document.getElementById('circle-' + (this.circleClicked));
+      const circleLabel = document.querySelector(`#circle-${this.circleClicked} .namelabeltext`);
+      const dotLabel = dot.querySelector('.namelabeltext') as HTMLElement;
+      if (circle && dot.style.display != "inline-block" && circleLabel && dotLabel) {
+        dot.style.display = "inline-block"
+        dot.style.backgroundImage = circle.style.backgroundImage;
+        dot.style.backgroundSize = 'contain';
+        dot.style.backgroundRepeat = 'no-repeat';
+        dot.style.backgroundPosition = 'center';
+        circle.style.display = "none";
+        dotLabel.textContent = circleLabel.textContent;
+        this.circleClicked = -1;
+        this.pieces--;
+      }
+    }
+    else {
+      this.dotClicked.forEach((clicked, index) => {
+        if (clicked) {
+          if (dot.style.display != "inline-block") {
+           
+            dot.style.display = "inline-block"
+            dot.style.backgroundImage = dots[index].style.backgroundImage;
+            dot.style.backgroundSize = 'contain';
+            dot.style.backgroundRepeat = 'no-repeat';
+            dot.style.backgroundPosition = 'center';
+            const dotLabelOld = dots[index].querySelector('.namelabeltext') as HTMLElement;
+            const dotLabelNew = dot.querySelector('.namelabeltext') as HTMLElement;
+            if (dotLabelNew && dotLabelOld) {
+              dotLabelNew.textContent = dotLabelOld.textContent;
+            }
+            dots[index].style.display = "none";
+            this.dotClicked[index] = false;
+          }
+        }
+      });
+    }
+  }
+  onPiecesClick(): void {
+    const dots: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+    const circle: HTMLElement | null = document.getElementById('circle-' + this.pieces);
+      this.dotClicked.forEach((clicked, index) => {
+        if (clicked && circle) {
+          if (circle.style.display != "inline-block") {
+            circle.style.display = "inline-block"
+            circle.style.backgroundImage = dots[index].style.backgroundImage;
+            circle.style.backgroundSize = 'contain';
+            circle.style.backgroundRepeat = 'no-repeat';
+            circle.style.backgroundPosition = 'center';
+            const circleLabel = document.querySelector(`#circle-${this.pieces} .namelabeltext`);
+            const dotLabel = dots[index].querySelector('.namelabeltext') as HTMLElement;
+            if (dotLabel && circleLabel) {
+              circleLabel.textContent = dotLabel.textContent;
+            }
+            dots[index].style.display = "none";
+            this.dotClicked[index] = false;
+            this.pieces++;
+          }
+        }
+      });
+    }
   // Update transform origin based on mouse position relative to the container
   updateTransformOrigin(event: WheelEvent): void {
     const container = event.currentTarget as HTMLElement;
@@ -147,8 +219,64 @@ export class AppComponent{
     const mouseYPercent = (this.mouseY / rect.height) * 100;
 
     // Set transform origin based on mouse position
-    this.transformOrigin = `${mouseXPercent}% ${mouseYPercent}%`;;
+    this.transformOrigin = `${mouseXPercent}% ${mouseYPercent}%`;
   }
   // Method to handle the scroll event
- 
+  clear(): void {
+    const dots: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+    for (var i = 0; i < this.value * this.value; i++) {
+      if (dots[i].style.display != 'none' && dots[i].style.display!="") {
+        const circle: HTMLElement | null = document.getElementById('circle-' + this.pieces);
+        const circleLabel = document.querySelector(`#circle-${this.pieces} .namelabeltext`);
+        const dotLabel = dots[i].querySelector('.namelabeltext') as HTMLElement;
+          if (circle) {
+            if (circle.style.display != "inline-block" && circleLabel && dotLabel) {
+              circle.style.display = "inline-block"
+              circle.style.backgroundImage = dots[i].style.backgroundImage;
+              circle.style.backgroundSize = 'contain';
+              circle.style.backgroundRepeat = 'no-repeat';
+              circle.style.backgroundPosition = 'center';
+              circleLabel.textContent = dotLabel.textContent;
+              this.pieces++;
+            }
+          }
+        dots[i].style.display = "none";
+      }
+    }
+  }
+
+  create(): void {
+    this.circles++;
+    if (this.previewUrl == null) {
+      this.imageUrl.push("");
+    }
+    setTimeout(() => {
+      const circle: HTMLElement | null = document.getElementById('circle-' + (this.circles - 1));
+      const circleLabel = document.querySelector(`#circle-${(this.circles - 1)} .namelabeltext`);
+      if (circle && circleLabel) {
+        if (this.imageUrl[this.circles-1]) {
+          circle.style.backgroundImage = `url(${this.imageUrl[this.circles - 1]})`;
+          circle.style.backgroundSize = 'contain';
+          circle.style.backgroundRepeat = 'no-repeat';
+          circle.style.backgroundPosition = 'center';
+          this.previewUrl = "";
+        }
+        circleLabel.textContent = this.name;
+        this.pieces++;
+      }
+    });
+  }
+
+  onImageSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      this.previewUrl = URL.createObjectURL(file);
+      this.imageUrl.push(this.previewUrl);
+    }
+  }
+  numSequence(n: number): Array<number> {
+    return Array(n);
+  }
+
 }
